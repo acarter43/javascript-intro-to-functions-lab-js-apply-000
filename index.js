7e20b74d44263558feb8723a083e7cb3418f4f7d
arianna carter <carterari43@gmail.com>
2018-05-26 05:33:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
-  if
+  if (string === "I love you, Grandma"){
+    return "I love you,too"
+  }
 }
