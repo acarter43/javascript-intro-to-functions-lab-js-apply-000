5a2f05d43d1f6340464431f1669d85da22fd8455
arianna carter <carterari43@gmail.com>
2018-05-26 05:31:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,8 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string ) {
     return "I can't hear you!"
   }
-  if (string.toUpperCase() === )
+  if (string.toUpperCase() === string){
+    return "YES INDEED!"
+  }
+  
 }
